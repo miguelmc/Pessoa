a7085cd9919157f83aebc6d17c4b5115b1b81873
Zechr <Zechr@users.noreply.github.com>
2016-04-28 18:30:21 -0400

    Updates to front end

--- articles.js
+++ articles.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     for (var j = 0; j < alphabet.length; j++) {
       $('#articles').append('<div class="abc-article-section" id="' + alphabet[j] + '"></div>');
     }
+    results = parseJSON(results);
     for (var i = 0; i < results.length; i++) {
       var id = results[i].title.substring(1).toLowerCase();
       $('#' + id).append('<div class="article"><a href="' + results[i].link + '"><img src="' + results[i].img + '" width="80%"></a>' + '<a href="' + results[i].link + '">' + results[i].title + '</a></div>');
--- single-article.js
+++ single-article.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  $.get('/article/' + $(document).find("title").text();, function(results) {
+  $.get('/article/' + $(document).find("title").text(), function(results) {
     $('.detail-article').append('<div id="detail-image"><img src="images/' + results.img + '" width="100%"></div>');
     $('.detail-article').append('<div id="detail-text"><a href="' + results.link + '">' + results.title + '</a>');
     $('#detail-text').append(results.abstract);
-  }
+  });
+  $.get('/article-images/' + $(document).find('title').text(), function(results) {
+    results = parseJSON(results);
+    for (var i = 0; i < results.length; i++) {
+      $('#image-grid').append('<div class="article-image"><a href="' + results[i].link + '"><img src="' + results[i].img + '" width="80%"></a></div>');
+    }
+  });
+    
 }
